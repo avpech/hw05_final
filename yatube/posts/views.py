06ef9c126a15482808c71c7cf142a36fab58c2f8
Артур Печенюк <velort777@yandex.ru>
2023-01-13 16:45:42 +0300

    Third review

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -157,7 +157,7 @@ def profile_follow(request, username):
 @login_required
 def profile_unfollow(request, username):
     author = get_object_or_404(User, username=username)
-    if Follow.objects.filter(user=request.user, author=author).exists():
-        follow = Follow.objects.get(user=request.user, author=author)
+    follow = Follow.objects.filter(user=request.user, author=author)
+    if follow.exists():
         follow.delete()
     return redirect('posts:profile', username)
